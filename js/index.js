31ebe5913c3fa4aaea822d8f434c7619c2e5b051
SamZuBeli <97295077+javabin7777777@users.noreply.github.com>
2022-08-27 19:10:48 -0300

    Add files via upload
    Atualização

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 
-function funcao(){   
+function funcao(){ 
     var a=document.getElementsByTagName("input")[0]; // nome
     sessionStorage.setItem('primeiroNome',a.value);  
     var b= document.getElementsByTagName("input")[1]; // sobrenome
@@ -16,46 +16,55 @@ function funcao(){
     sessionStorage.setItem('experiencia',a.value);
     
     a=document.getElementsByTagName("input")[5];   
-    if(a.value == ""){
-        sessionStorage.setItem('youtube',"#base");               
-    }else{        
+    if(checarUrl(a.value)){
         sessionStorage.setItem('youtube',a.value);
-    }   
+    }else{
+        sessionStorage.setItem('youtube',"#base");
+    }      
 
     a=document.getElementsByTagName("input")[6];
-    if(a.value == ""){
-        sessionStorage.setItem('github',"#base");               
+    if(checarUrl(a.value)){
+        sessionStorage.setItem('github',a.value);               
     }else{        
-        sessionStorage.setItem('github',a.value);
+        sessionStorage.setItem('github',"#base");
     }     
 
     a=document.getElementsByTagName("input")[7];
-    if(a.value == ""){
-        sessionStorage.setItem('linkedin',"#base");
-    }else{
+    if(checarUrl(a.value )){
         sessionStorage.setItem('linkedin',a.value);
+    }else{
+        sessionStorage.setItem('linkedin',"#base");
     }   
 
     a=document.getElementsByTagName("input")[8];
-    if(a.value == ""){
-        sessionStorage.setItem('facebook',"#base");
-    }else{
+    if(checarUrl(a.value)){
         sessionStorage.setItem('facebook',a.value);
+    }else{
+        sessionStorage.setItem('facebook',"#base");
     }    
 
     a=document.getElementsByTagName("input")[9];
-    if(a.value == ""){
-        sessionStorage.setItem('instagram',"#base");
-    }else{
+    if(checarUrl(a.value)){
         sessionStorage.setItem('instagram',a.value);
+    }else{
+        sessionStorage.setItem('instagram',"#base");
     }    
 
     a=document.getElementsByTagName("input")[10];
-    if(a.value == ""){
-        sessionStorage.setItem('twitter',"#base");
-    }else{
+    if(checarUrl(a.value)){
         sessionStorage.setItem('twitter',a.value);
+    }else{
+        sessionStorage.setItem('twitter',"#base");
     } 
 
     window.open("cartaodevisita.html","_blank");
+
+    function checarUrl(string){ // checa a url digitada no campo input
+        try{
+             let url=new URL(string);
+             return true;
+        }catch(e){
+             return false;            
+        }
+     } 
 }
